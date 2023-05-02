f5598858eb6a61eb060dedffdc4da404df2b3733
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-03 00:53:01 +0300

    add all hw on main branche

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//first home work calculator :D
+const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
+const firstnumber = Number(prompt('Please enter the first number: '));
+const secondnumber = Number(prompt('Please, enter the second number: '));
+let result;
+if (operator === '+'){
+    result = firstnumber + secondnumber;
+    alert(`${firstnumber} + ${secondnumber} = ${result}`);
+} else if (operator === '-'){
+    result = firstnumber - secondnumber;
+    alert(`${firstnumber} - ${secondnumber} = ${result}`);
+} else if (operator === "*"){
+    result = firstnumber * secondnumber;
+    alert(`${firstnumber} * ${secondnumber} = ${result}`);
+}else if(operator === "/"){
+    result = firstnumber / secondnumber;
+    alert(`${firstnumber} / ${secondnumber} = ${result}`);
+}else{
+    alert(`Please try again`);
+}
+
+//second home work average arithmetic
 const firstNumber = Number(prompt('Please send a first number'));
 const secondNumber = Number(prompt('Please send a second number'));
 const treeNumber = Number(prompt('Please send a tree number'));
