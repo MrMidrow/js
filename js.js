8ff1f73c9d1894342d656e3a880804e40f78c25a
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-02 11:08:42 +0300

    average arithmetic

---
+++ js.js
@@ -0,0 +1,5 @@
+const firstNumber = Number(prompt('Please send a first number'));
+const secondNumber = Number(prompt('Please send a second number'));
+const treeNumber = Number(prompt('Please send a tree number'));
+const answer = (firstNumber + secondNumber + treeNumber) / 3;
+alert(`(${firstNumber} + ${secondNumber} + ${treeNumber}) / 3 = ${answer}`)
