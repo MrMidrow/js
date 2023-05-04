11f04b186892df3c26ce04b999918c7fbfcf744a
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-04 12:59:57 +0300

    chencged 'Number()' on '+'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //first home work calculator :D
 const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
-const firstnumber = Number(prompt('Please enter the first number: '));
-const secondnumber = Number(prompt('Please, enter the second number: '));
+const firstnumber = +prompt('Please enter the first number: ');
+const secondnumber = +prompt('Please, enter the second number: ');
 let result;
 if (operator === '+'){
     result = firstnumber + secondnumber;
@@ -20,8 +20,8 @@ if (operator === '+'){
 }
 
 //second home work average arithmetic
-const firstNumber = Number(prompt('Please send a first number'));
-const secondNumber = Number(prompt('Please send a second number'));
-const treeNumber = Number(prompt('Please send a tree number'));
+const firstNumber = +prompt('Please send a first number');
+const secondNumber = +prompt('Please send a second number');
+const treeNumber = +prompt('Please send a tree number');
 const answer = (firstNumber + secondNumber + treeNumber) / 3;
 alert(`(${firstNumber} + ${secondNumber} + ${treeNumber}) / 3 = ${answer}`)
