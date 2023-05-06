4363d3f5fc403fd3dfe499626ff6a405eefee6e1
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-06 12:41:34 +0300

    add home work 3

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-//first home work calculator :D
-const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
-const firstnumber = +prompt('Please enter the first number: ');
-const secondnumber = +prompt('Please, enter the second number: ');
-let result;
-if (operator === '+'){
-    result = firstnumber + secondnumber;
-    alert(`${firstnumber} + ${secondnumber} = ${result}`);
-} else if (operator === '-'){
-    result = firstnumber - secondnumber;
-    alert(`${firstnumber} - ${secondnumber} = ${result}`);
-} else if (operator === "*"){
-    result = firstnumber * secondnumber;
-    alert(`${firstnumber} * ${secondnumber} = ${result}`);
-}else if(operator === "/"){
-    result = firstnumber / secondnumber;
-    alert(`${firstnumber} / ${secondnumber} = ${result}`);
+const age = +prompt('How old are you? Please, write in number');
+const city = prompt('where are you from?');
+
+if (age === NaN || age < 0 || age === 0){
+    alert("Too bad you didn't want to enter yours age")
+} 
+if(city !== null){
+    switch(city){
+        case "Kiev" && "kiev" :
+            alert(`Your age ${age}
+You live in capital Ukraine ${city}`)
+            break
+        case "Washington" && "washington" :
+            alert(`Your age ${age}
+You live in capital USA ${city}`)
+            break
+        case "London" && "london" :
+            alert(`Your age ${age}
+You live in capital great britan ${city}`)
+            break
+        default:
+            alert(`Your age ${age}
+You live in city ${city}`)
+            break
+    }
 }else{
-    alert(`Please try again`);
+    alert("Too bad you didn't want to enter yours city");
 }
 
-//second home work average arithmetic
-const firstNumber = +prompt('Please send a first number');
-const secondNumber = +prompt('Please send a second number');
-const treeNumber = +prompt('Please send a tree number');
-const answer = (firstNumber + secondNumber + treeNumber) / 3;
-alert(`(${firstNumber} + ${secondNumber} + ${treeNumber}) / 3 = ${answer}`)
+const sport = prompt('What sport do you do? Football, basketball or box?')
+if(sport !== null){
+    switch(sport){
+        case 'Football' || 'football':
+            alert('Сool! Do you want to be like Lionel Messi')
+            break
+        case 'basketball' || 'Basketball':
+            alert('Сool! Do you want to be like Michael Jordan')
+            break
+        case 'box' || 'Box':
+            alert('Сool! Do you want to be like Vladimir Klichko')
+            break
+        default:
+            ("Too bad you didn't want to enter yours sport")
+            break
+    };
+}
+
+
+
+// age === NaN ? alert("Too bad you didn't want to enter yours age"): alert();
+// city === null ? alert("Too bad you didn't want to enter yours city"):alert();
+// sport === null ? alert("Too bad you didn't want to enter yours sport"): alert();
