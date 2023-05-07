d4e73d4330513f324cc72f61edb58927fc4a947d
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-07 14:44:50 +0300

    mini fix code

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let age = +prompt('What year of birth? 1990 or other');
 let city = prompt('Where are you from?');
 let sport = prompt('What sport do you do? Football, basketball or box?');
 
-if (age === NaN || age < 0 || age === 0 || age === ""){
+if (age !== NaN || age < 0 || age === 0 || age === ""){
     age = "Too bad you didn't want to enter yours age";
 }else{
     age = `You age ${2023 - age} years old`;
 }
 
-if(city !== null && city !== ""){
+if(typeof city === "string" || city !== null && city !== ""){
     switch(city.toLowerCase()){
         case "":
             break
