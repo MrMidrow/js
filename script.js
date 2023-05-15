0a1545d46c1d3756b82e63abc149a10122798cb5
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-15 19:14:21 +0300

    fix step 3

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ if(!number){
 	for(let i = 1; i <= number; i++){
    massage.push(prompt('send a massage: '))
   }
+  alert(massage)
+  //step 2
+  massage.sort();
+  alert(massage)
 }
-alert(massage)
-//step 2
-massage.sort();
-alert(massage)
 //step 3
-if(massage.length <= 2){
+if(massage.length < 2){
     alert('More than 2 elements are required to remove elements from an array')
 }else{
     massage.splice(1, 3)
-    alert(sortMassage)
+    alert(massage)
 }
