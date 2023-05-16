7fdcaeccc093e41217068d280fb8da6ee84e1913
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-16 20:15:06 +0300

    add generateKey

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-//first home work calculator :D
-const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
-const firstnumber = +prompt('Please enter the first number: ');
-const secondnumber = +prompt('Please, enter the second number: ');
-let result;
-if (operator === '+'){
-    result = firstnumber + secondnumber;
-    alert(`${firstnumber} + ${secondnumber} = ${result}`);
-} else if (operator === '-'){
-    result = firstnumber - secondnumber;
-    alert(`${firstnumber} - ${secondnumber} = ${result}`);
-} else if (operator === "*"){
-    result = firstnumber * secondnumber;
-    alert(`${firstnumber} * ${secondnumber} = ${result}`);
-}else if(operator === "/"){
-    result = firstnumber / secondnumber;
-    alert(`${firstnumber} / ${secondnumber} = ${result}`);
-}else{
-    alert(`Please try again`);
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(numbers, key){
+	let password = '';
+    for(let i = 0; i < numbers; i++){
+       password +=	key.charAt(Math.floor(Math.random()*key.length))
+    }
+    return password
 }
 
-//second home work average arithmetic
-const firstNumber = +prompt('Please send a first number');
-const secondNumber = +prompt('Please send a second number');
-const treeNumber = +prompt('Please send a tree number');
-const answer = (firstNumber + secondNumber + treeNumber) / 3;
-alert(`(${firstNumber} + ${secondNumber} + ${treeNumber}) / 3 = ${answer}`)
+const key = generateKey(16, characters);
+console.log(key); 
