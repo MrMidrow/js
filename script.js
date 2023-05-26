f5be46de4d0d61280f568950c0315d12a9cdd406
Mr.Midrow <jeyjeykey@icloud.com>
2023-05-26 09:43:49 +0300

    reload main

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let ladder = {
-    step: 0,
-    up() {
-        this.step++;
-        return this
-    },
-    down() {
-        this.step--;
-        return this
-    },
-    showStep() { // показывает текущую ступеньку
-        alert(this.step);
-        return this
-    }
-};
+//first home work calculator :D
+const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
+const firstnumber = +prompt('Please enter the first number: ');
+const secondnumber = +prompt('Please, enter the second number: ');
+let result;
+if (operator === '+'){
+    result = firstnumber + secondnumber;
+    alert(`${firstnumber} + ${secondnumber} = ${result}`);
+} else if (operator === '-'){
+    result = firstnumber - secondnumber;
+    alert(`${firstnumber} - ${secondnumber} = ${result}`);
+} else if (operator === "*"){
+    result = firstnumber * secondnumber;
+    alert(`${firstnumber} * ${secondnumber} = ${result}`);
+}else if(operator === "/"){
+    result = firstnumber / secondnumber;
+    alert(`${firstnumber} / ${secondnumber} = ${result}`);
+}else{
+    alert(`Please try again`);
+}
 
-ladder.up().up().down().showStep();
+//second home work average arithmetic
+const firstNumber = +prompt('Please send a first number');
+const secondNumber = +prompt('Please send a second number');
+const treeNumber = +prompt('Please send a tree number');
+const answer = (firstNumber + secondNumber + treeNumber) / 3;
+alert(`(${firstNumber} + ${secondNumber} + ${treeNumber}) / 3 = ${answer}`)
