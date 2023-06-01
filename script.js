a51135594afb98502999eb9a0ebdd81b3452153f
Mr.Midrow <jeyjeykey@icloud.com>
2023-06-01 22:46:22 +0300

    change event listener

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const textArea = document.getElementById('text');
 
-textArea.onmouseover = textArea.onmouseout = mouse ;
+textArea.addEventListener('mouseover', (event)=>{
+    const div = document.createElement("div");
+    div.setAttribute('id','box')
+    div.style.cssText = `
+    margin-left: 20px;
+    width: 50px;
+    height: 50px;
+    border: 2px solid red;
+    background: black;`  
+    //or div.classList.add('box')
+    event.target.after(div)
+})
 
-function mouse(event){
-	if(event.type == 'mouseover'){
-      const div = document.createElement("div");
-      div.setAttribute('id','box')
-      div.style.cssText = `
-      margin-left: 20px;
-      width: 50px;
-      height: 50px;
-      border: 2px solid red;
-      background: black;`  
-      //or div.classList.add('box')
-			event.target.after(div)
-      console.log(event.target.type)
-  }
-  if(event.type == 'mouseout'){
-  		document.getElementById('box').remove();
-  }
-}
+textArea.addEventListener('mouseout', ()=>{
+		document.getElementById('box').remove();
+})
