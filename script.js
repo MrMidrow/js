3dd1822fc2399e1224d750ba5dfda72b8aebc62d
Mr.Midrow <jeyjeykey@icloud.com>
2023-06-01 19:15:03 +0300

    add change in project

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let position = 0;
-const sliders = document.querySelector('#sliders')
+const sliders = document.querySelector('#sliders');
+const btn_next = document.querySelector('#next_slider');
+const btn_prev = document.querySelector('#prev_slider');
 
-
-document.querySelector('#next_slider').addEventListener('click', ()=>{
-        position += 488 ;
-        if(position > 976){
-            position = 0;
-        }
-        sliders.style.left = -position + 'px'
+btn_next.addEventListener('click', ()=>{
+    position += 488 ;
+    if(position > 976){
+        // position = 0;
+    }
+    sliders.style.left = -position + 'px'
 })
 
-document.querySelector('#prev_slider').addEventListener('click', ()=>{
+btn_prev.addEventListener('click', ()=>{
     position -= 488 ;
     if(position < 0){
-        position = 976;
+        // position = 976;
     }
     sliders.style.left = -position + 'px'
 })
