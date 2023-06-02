fb54ab309c7c1dd3822e42c89655a14b77230677
Mr.Midrow <jeyjeykey@icloud.com>
2023-06-02 19:54:04 +0300

    added carousel

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const sliders = document.querySelector('#sliders');
 const btn_next = document.querySelector('#next_slider');
 const btn_prev = document.querySelector('#prev_slider');
 
+btn_prev.style.visibility = 'hidden'
+
 btn_next.addEventListener('click', ()=>{
     position += 488 ;
-    if(position > 976){
-        // position = 0;
-    }
     sliders.style.left = -position + 'px'
+    searchClickOnButton()
 })
 
 btn_prev.addEventListener('click', ()=>{
     position -= 488 ;
-    if(position < 0){
-        // position = 976;
-    }
     sliders.style.left = -position + 'px'
+    searchClickOnButton()
 })
+
+function searchClickOnButton(){
+  	position !== 0 ? btn_prev.style.visibility = 'visible' : btn_prev.style.visibility = 'hidden'
+    position === 976 ? btn_next.style.visibility = 'hidden' : btn_next.style.visibility = 'visible'
+}
