fe048ea371277fcb2abec2954eb62f6fb854b564
Mr.Midrow <jeyjeykey@icloud.com>
2023-06-21 09:46:43 +0300

    finish hw_14

--- script.js
+++ script.js
@@ -2,143 +2,62 @@
 const btnBuyItem = document.querySelector('#buyItem')
 const formBuy = document.querySelector('#form_buy')
 
-//inputs
-const first_name = document.querySelector('#firstName')
-const second_name = document.querySelector('#secondName')
-const optionCity = document.querySelector('#city')
-const mail = document.querySelector('#mail');
-const paidText = document.querySelector("#methodPaid");
-const quantityInput = document.querySelector("#quantity");
-const addProduct = document.querySelector('#added');
-const subtractProduct = document.querySelector('#subtract');
-const textNode = document.querySelector('#text_node')
-//inputs
+const quantity = document.querySelector('#quantity')
+const worningText = document.querySelector('#worning')
+let user = []
 
+worningText.style.cssText = `padding: 15px;
+margin-top: 10px;
+background: red;
+color: white;
+border: 0.5px solid grey`;
+
+const makeUnic = (arr) => [...new Set(arr)];
 
 btnBuyItem.addEventListener('click', ()=>{
-    formBuy.style.visibility !== 'visible' ? formBuy.style.visibility = 'visible' : formBuy.style.visibility = 'hidden'
+    formBuy.style.visibility !== 'visible' ? formBuy.style.visibility = 'visible' : formBuy.style.visibility = 'hidden';
 });
 
-addProduct.addEventListener('click', ()=>{
-    quantityInput.value++;
-});
+formBuy.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-subtractProduct.addEventListener('click', ()=>{
-    quantityInput.value--;
-});
-
-function errorRedText(){
-    paidText.style.color = 'red';
-}
-
-function normalizedColorText(){
-    paidText.style.color = 'black'
-};
-
-function worning(){
-    let div, p, text;
-    div = document.querySelector('#worning')
-    div.classList.add = 'worning'
-    p = document.createElement('p')
-    text = document.createTextNode('please enter all grap')
-    p.appendChild(text)
-    div.appendChild(p)
-    formBuy.appendChild(div)
-}
-
-function formResoultValues(){
-    const formValues = new FormData(formBuy)
-    let objFormData = {}
-    for(let newArr of formValues){
-        objFormData[newArr[0]] = newArr[1]
-    }
-    return objFormData
-}
-
-function formValidator(formResoultValues){
-    let newobj = formResoultValues
-    console.log(newobj)
-    for(let exelentObj of newobj){
-        if(exelentObj[1] == '' || exelentObj[1] == null) {
-            console.log(`pleas enter a ${exelentObj[0]}`)
-            return false
-        }
-    }
-}
-
-formBuy.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    // auditAndGetInputValue()
-    let formObj = formResoultValues()
-    addResaultOnPage(formObj,formValidator())
-    if(addResaultOnPage() == false){
-        console.log('bad boy')
-    }
-
-
-});
-
-function auditInputValue(){
-    if(!first_name.value){
-        first_name.style.borderColor = 'red'
-    }else{
-        first_name.style.borderColor = ``;
-    }
-
-    if(!second_name.value){
-        second_name.style.borderColor = 'red'
-    }else{
-        second_name.style.borderColor = ''
-    }
-
-    if(!optionCity.value){
-        optionCity.style.borderColor = 'red'
-    }else{
-        optionCity.style.borderColor = ''
-    }
-
-    if(!mail.value){
-        mail.style.borderColor = 'red'
-    }else{
-        mail.style.borderColor = '';
-    }
-
-    let postpaid = formBuy.postPay.checked;
-    let nawpaid = formBuy.nawPay.checked;
-    if(postpaid != true && nawpaid != true){
-        paidText.style.color = 'red';
-    }else if(postpaid == true){
-        paidText.style.color = ''
-    }else if(nawpaid == true){
-        paidText.style.color = ''
-    }
-
-    if(quantityInput.value <= 0){
-        document.querySelector('#amountText').style.color = 'red'
-    }else{
-        document.querySelector('#amountText').style.color = ''
-    }
-
-}
-
-function addResaultOnPage(objValue, obj){
-
-    if(obj == true){
-        const div = document.createElement('div')
-        div.style.cssText = `
-        display: flex;
-        width: 320px;
-        heigth: 150px
-        border: 2px solid black;
-        background: grey`;
-        let p = document.createElement('p')
-        console.log(obj)
-        let textNode = document.createTextNode(`${obj.first_name}, ${obj.second_name}, ${obj.city}, ${obj.mail}, ${obj.paid}, ${obj.how_much}, ${obj.textNode}`)
-        p.appendChild(textNode)
-        div.appendChild(p)
-        document.body.append(div)
-    }else{
-        auditInputValue()
-    }
+    let userValue = makeUnic(user);
     
-}
+    if(userValue.length >= 6){
+        formBuy.style.display = 'none';
+        addResaultOnPage();
+    }else{
+        worningText.textContent = 'please enter all strings';
+    };
+});
+
+formBuy.addEventListener('input', (e) => {
+    let value = e.target.value;
+    let inputName = e.target.name;
+
+    if(value.trim() !== ''){
+        user.push(inputName);
+    }
+});
+
+function addResaultOnPage(){
+    const div = document.querySelector('div')
+    div.style.cssText = `
+    display: flex;
+    margin-top: 10px;
+    width: 320px;
+    heigth: 150px;
+    border: 2px solid black;
+    background: grey`;
+    let p = document.createElement('p');
+    p.textContent = `You get a ${document.querySelector('h3').textContent}, 
+    ${document.querySelector('.descriptionItem').textContent}, 
+    Your full name ${formBuy.first_name.value} ${formBuy.second_name.value},  
+    City: ${formBuy.city.value},  
+    Address new mail: ${formBuy.mail.value},  
+    Method paid: ${formBuy.paid.value},  
+    Amount: ${formBuy.how_much.value},  
+    Comment: ${formBuy.textNode.value}`;
+    div.appendChild(p);
+    document.querySelector('.get_address').append(div);
+};
