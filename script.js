8ad4574601383c79960f7876d0dad3a620dc7cd8
Mr.Midrow <jeyjeykey@icloud.com>
2023-07-13 19:11:25 +0300

    done hw_19

--- script.js
+++ script.js
@@ -1,27 +1,83 @@
-//first home work calculator :D
-const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
-const firstnumber = +prompt('Please enter the first number: ');
-const secondnumber = +prompt('Please, enter the second number: ');
-let result;
-if (operator === '+'){
-    result = firstnumber + secondnumber;
-    alert(`${firstnumber} + ${secondnumber} = ${result}`);
-} else if (operator === '-'){
-    result = firstnumber - secondnumber;
-    alert(`${firstnumber} - ${secondnumber} = ${result}`);
-} else if (operator === "*"){
-    result = firstnumber * secondnumber;
-    alert(`${firstnumber} * ${secondnumber} = ${result}`);
-}else if(operator === "/"){
-    result = firstnumber / secondnumber;
-    alert(`${firstnumber} / ${secondnumber} = ${result}`);
-}else{
-    alert(`Please try again`);
+
+const form = document.querySelector('#searchPostId');
+const commentsBtn = document.querySelector('#searchComment');
+const blockComments = document.querySelector('#comment');
+const userId = document.querySelector('.post');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const items = document.querySelectorAll('.items');
+    const id = +form.Post.value;
+    if(items.length > 0){
+        items.forEach((e) => e.remove());
+    }
+    commentsBtn.removeAttribute('disabled');
+    !id ? console.log('Error') : getPost(id);
+})
+
+async function getPost(id) {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    try{
+        const requestData = await (await fetch(url)).json();
+        const post = requestData[id - 1];
+        if(id > requestData.length){
+            throw 'Введено не коректное число';
+        }else{
+            renderingPost(post);
+        }
+    }catch(error){
+        console.error(error)
+    }
 }
 
-//second home work average arithmetic
-const firstNumber = +prompt('Please send a first number');
-const secondNumber = +prompt('Please send a second number');
-const treeNumber = +prompt('Please send a tree number');
-const answer = (firstNumber + secondNumber + treeNumber) / 3;
-alert(`(${firstNumber} + ${secondNumber} + ${treeNumber}) / 3 = ${answer}`)
+function renderingPost(data) {
+    userId.setAttribute('data-id', `${data.userId}`);
+    document.querySelector('.userName').textContent = `${data.userId}`;
+    document.querySelector('.title').textContent = `${data.title}`;
+    document.querySelector('.body').textContent = `${data.body}`;
+    commentsBtn.style.visibility = 'visible';
+}
+
+commentsBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let id = userId.getAttribute('data-id');
+    getUserComment(id);
+    blockComments.style.visibility = 'visible'
+    commentsBtn.setAttribute('disabled', 'true')
+})
+
+async function getUserComment(id){
+    const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`
+    try{
+        const data = await (await fetch(url)).json();
+        data.forEach((e)=>{
+            if(!document.querySelector(`.item-${e.id}`)) createItemComment(e)
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
+
+function createItemComment(e){
+    const item = document.createElement('div');
+    item.classList.add(`item-${e.id}`)
+    item.classList.add(`items`)
+
+    const name = document.createElement('h3');
+    name.classList.add('name');
+    name.textContent = `${e.postId}`;
+
+    const mail = document.createElement('a');
+    mail.classList.add('mail');
+    mail.setAttribute('href', e.mail);
+    mail.textContent = `${e.email}`;
+
+    const body = document.createElement('p');
+    body.classList.add('bodyComment');
+    body.textContent = `${e.body}`;
+
+    item.appendChild(name);
+    item.appendChild(mail);
+    item.appendChild(body);
+    blockComments.appendChild(item);
+}
